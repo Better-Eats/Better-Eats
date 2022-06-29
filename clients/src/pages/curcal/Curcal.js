9deaae6ca12bca812e6f8171d73363b93e821d51
Joshua Ham <Joshuaju.ham@gmail.com>
2022-06-29 09:25:31 -0700

    for merging

--- clients/src/pages/curcal/Curcal.js
+++ clients/src/pages/curcal/Curcal.js
@@ -1,13 +1,10 @@
 import './curcal.css'
-import {useNavigate} from 'react-router-dom';
 
 export default function Curcal(){
-  let navigate = useNavigate();
 
   return (
     <div className ='curcal'>
       current cal page
-      <button onClick={() => {navigate('/profile')}}>navigate</button>
     </div>
 
   )
