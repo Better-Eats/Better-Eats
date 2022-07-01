e430b7c778515c8ee8bcdac61605589730e7e54b
Fan <aliciafzhang@gmail.com>
2022-07-01 11:51:59 -0700

    cart info add to cart

--- clients/src/components/cart/CartItems.js
+++ clients/src/components/cart/CartItems.js
@@ -3,23 +3,24 @@ import {useState} from 'react';
 
 export default function CartItems({item}){
   const [count, setCount] = useState(item["count"]);
-  console.log("item", item);
+  console.log("count", count);
+
   return(
     <div>
     { item.count === 0? (<div></div>) : (<div className="Cart-Items">
     <div className="image-box">
-    <img src={item.image} alt="" style={{ height: '120px' }} />
+    <img src={item.url || item.image} alt="" style={{ height: '120px' }} />
     </div>
     <div className="about">
-      <h1 className="title">{item.name}</h1>
-      <h3 className="subtitle">{item.dietLabel}</h3>
+      <h1 className="title">{item.description ||item.name}</h1>
+      <h3 className="subtitle">Serving Size: {item.servingSize}</h3>
     </div>
     <div className="counter">
-      <div className="btn" onClick={() =>setCount(count+1)}>+</div>
-      <div className="count">{item.count}</div>
       <div className="btn"  onClick={() => setCount(count-1)}>-</div>
+      <div className="count">{count >= 0 ? count : 0}</div>
+      <div className="btn" onClick={() =>setCount(count+1)}>+</div>
     </div>
-    <div className="prices">${item.calories}</div>
+    <div className="prices">{item.calories} calories</div>
     </div>)}
     </div>
   )
